0055c7440496e162fd0403d059cb8b0389447989
Manoj Wajekar <manojwajekar93@gmail.com>
2017-12-11 20:03:12 -0500

    Merged code: Added Reviews tables and reviews form part

--- imports/api/servercode.js
+++ imports/api/servercode.js
@@ -6,9 +6,11 @@ export const Ilnesses = new Mongo.Collection('illness');
 //load Remedy collection
 export const Remedy = new Mongo.Collection('remedy');
 
-//load Remedy collection
+//load products collection
 export const Products = new Mongo.Collection('products');
 
+//load reviews collection
+export const Reviews = new Mongo.Collection('reviews');
 //publish meteor queries here...
 if (Meteor.isServer) {
     // This code only runs on the server
@@ -22,5 +24,34 @@ if (Meteor.isServer) {
 
     Meteor.publish('illness', function illnessPublication() {
         return Ilnesses.find({});
+    });
+
+        Meteor.publish('reviews', function reviewsPublication() {
+        return Reviews.find({});
+    });
+
+    // Method to get aggregate rating
+    // TODO: Need to fix it
+    Meteor.methods({ 'getRatingServer'(Remedy_id){
+
+            return Reviews.aggregate([
+                {$match: {"Remedy_ID": Remedy_id}},
+                {
+                    $group: {
+                        _id: null,
+                        avgRating: {
+                            $avg: "$Rating"
+                        }
+                    }
+                }]);
+        }});
+
+    // Give insert permission to reviews table
+    Reviews.allow({
+        'insert': function (userId,doc) {
+            /* user and doc checks ,
+            return true to allow insert */
+            return true;
+        }
     });
 }
